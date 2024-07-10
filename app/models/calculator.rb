6eb917d973f07929130b9c5d95799f5e094d415d
kanya01 <kanyamosesm@gmail.com>
2024-07-10 12:01:35 +0100

    Update calculator.rb

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -5,6 +5,8 @@ class Calculator
         { threshold: 70000, rate: 0.3 },
         { threshold: 180000, rate: 0.33 },
         { threshold: Float::INFINITY, rate: 0.39 }
+     #Each threshold corresponds to the upper limit of that tax bracket, and the rates are cumulative 
+     #this way we can handle any income beyond the threshold.  
     ]
     
     def initialize(income)
@@ -21,10 +23,8 @@ class Calculator
           tax += taxable_income * bracket [:rate]
           previous_threshold = bracket[:threshold]
 
-        # if @income > bracket[:threshold]
-        #   taxable_income = [@income, bracket[:threshold]].min - previous_threshold
-        #   tax += taxable_income * bracket [:rate]
-        #   previous_threshold = bracket[:threshold]
+          #here we iterate through each tax bracket checking if if @income is greater than previous_threshold 
+          #taxable income is therefore calculated using the minimum of @income and the current bracket's threshold minus previous_threshold.
 
         else
             break
@@ -34,5 +34,6 @@ class Calculator
       tax
     end
 end
+
 
 
